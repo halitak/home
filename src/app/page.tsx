9e51b703a105b073a3ff43f8da114de1029b0c12
Halit Ak <e.halitak@gmail.com>
2023-10-18 01:02:01 +0300

    welcome home

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="p-4 flex justify-center items-center h-screen">
+      <h1 className="text-center text-6xl lg:text-9xl">Welcome home!</h1>
+    </main>
+  );
+}
